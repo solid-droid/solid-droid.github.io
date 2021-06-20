037e6cda485626b5dc0c02024588818f3053faa5
Nikhil M Jeby <32586986+solid-droid@users.noreply.github.com>
2021-06-20 22:05:51 +0530

    snap scroll

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-  
-   const body =document.getElementsByTagName("BODY")[0]
+   let position;
+   let scrollInProgress = false;
+
    function openFullscreen() {
-    // if (body.requestFullscreen) {
-    //     body.requestFullscreen();
-    // } else if (body.webkitRequestFullscreen) { /* Safari */
-    //     body.webkitRequestFullscreen();
-    // } else if (body.msRequestFullscreen) { /* IE11 */
-    //     body.msRequestFullscreen();
-    // }
+    if (document.body.requestFullscreen) {
+        document.body.requestFullscreen();
+    } else if (document.body.webkitRequestFullscreen) { /* Safari */
+        document.body.webkitRequestFullscreen();
+    } else if (document.body.msRequestFullscreen) { /* IE11 */
+        document.body.msRequestFullscreen();
+    }
   }
 
   $(document).ready(function(){
+    position = $(window).scrollTop(); 
     $('body').css({overflow:"hidden"});
     $(this).scrollTop(0);
 });
@@ -43,7 +45,7 @@ boxTl.to('.box', {duration:0.5, width:"25vw", delay: 0.1, ease: "power4.inOut"})
 
 
 words.forEach((word,i) => {
-  let tl = gsap.timeline({repeat:  i<words.length-1?1:0, yoyo: i<words.length-1?true:false, repeatDelay:1})
+  let tl = gsap.timeline({repeat:  i<words.length-1?1:0, yoyo: i<words.length-1?true:false, repeatDelay:0.7})
   tl.to('.text', {duration: 1, text: word})
   if(i==words.length-1){
     tl.to(".Name", {duration:1, y:"-20vh", onComplete:loadComplete},"-=1")
@@ -65,31 +67,40 @@ const screens={
 function loadComplete(){
     Page1();
     $('body').css({'overflow-y':'scroll'});
-    for(let i = 0; i<7 ;++i)
-        createScrollTriggers(i);
+
+    //not Full screen
+    $(window).scroll(function() {
+        scrollEvent(window);
+    });
+
+    //Full-screen
+    $(document.body).scroll(function() {
+        scrollEvent(document.body);
+    });
      
 }
 
-function createScrollTriggers(screen){
-    ScrollTrigger.create({
-        trigger: screens[screen],
-        start: "+=50 top", 
-        end: "-=50 bottom", 
-        onEnter: () => {
-            gsap.to(body, {duration: 1, scrollTo: screens[screen>=6?6:screen+1]})
-        },
-
-      });
-
-    ScrollTrigger.create({
-        trigger: screens[screen],
-        start: "-=50 bottom", 
-        end: "+=50 top", 
-        onEnterBack:() =>{
-            gsap.to(body, {duration: 1, scrollTo: screens[screen<=0?0:screen-1]})
+function scrollEvent(element){
+    var scroll = $(element).scrollTop();
+    if(!scrollInProgress){
+        if(scroll > position) {
+            // down
+            currentScreen = currentScreen >= 6 ? 6 : ++currentScreen;
+            console.log("down")
+            gsap.to("body", {duration: 0.5, scrollTo: screens[currentScreen], ease: "power2"});
+        } else {
+            // up
+            console.log("up")
+            currentScreen = currentScreen <= 0 ? 0 : --currentScreen
+            gsap.to("body", {duration: 0.5, scrollTo: screens[currentScreen], ease: "power2"});
         }
-
-      });
+    }
+    scrollInProgress = true;
+    position = scroll;
+    clearTimeout($.data(this, 'scrollTimer'));
+    $.data(this, 'scrollTimer', setTimeout(function() {
+        scrollInProgress = false;
+    }, 250));
 }
 
 function Page1(){
