57c7606d8c9e054cb7641c5ad3ff81306914451d
Nikhil M Jeby <32586986+solid-droid@users.noreply.github.com>
2021-06-21 15:53:43 +0530

    Added watch css

--- app.js
+++ app.js
@@ -9,6 +9,44 @@
   }
 
 
+  function updateClock(hours, minutes, seconds) {
+
+    var hourDegrees = hours * 30;
+    var minuteDegrees = minutes * 6;
+    var secondDegrees = seconds * 6;
+  
+    $('.hour-hand').css({
+      'transform': `rotate(${hourDegrees}deg)`
+    });
+  
+    $('.minute-hand').css({
+      'transform': `rotate(${minuteDegrees}deg)`
+    });
+  
+    $('.second-hand').css({
+      'transform': `rotate(${secondDegrees}deg)`
+    });
+  
+  }
+  
+  setClockWithCurrentTime();
+  
+  function setClockWithCurrentTime() {
+      var date = new Date();
+  
+    var hours = ((date.getHours() + 11) % 12 + 1);
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+  
+    updateClock(hours, minutes, seconds);
+  }
+  
+  
+  
+  setInterval(setClockWithCurrentTime, 1000);
+  
+
+
   $(document).ready(function(){
     $(this).scrollTop(0);
     $('body').css({overflow:"hidden"});
@@ -103,7 +141,7 @@ function BeginScreen(id){
 }
 
 function project1(id){
-    TimeLines[id].to(".rocket",{x:500 , duration:1});
+    // TimeLines[id].to(".rocket",{x:500 , duration:1});
 }
 
 function project2(){
